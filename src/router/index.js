8c6e3a1f80ca8c5e2b90014de673c7392c4eba14
seoh02h <seok02h@khu.ac.kr>
2022-08-29 23:16:48 +0900

    Add vue-router and views

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
+import OnlineStore from "@/views/OnlineStore.vue";
+import OfflineStore from "@/views/OfflineStore.vue";
+import Youtube from "@/views/Youtube.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: "history",
+    routes:[
+        {
+            name: "home",
+            path: "/",
+            component: Home,
+        },
+        {
+            name: "onlineStore",
+            path: "/online-store",
+            component: OnlineStore,
+        },
+        {
+            name: "oflineStore",
+            path: "/offline-store",
+            component: OfflineStore,
+        },
+        {
+            name: "youtube",
+            path: "/youtube",
+            component: Youtube,
+        },
+    ],
+})
+
+export default router;
---
+++ src/views/OfflineStore.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Offline Store</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
